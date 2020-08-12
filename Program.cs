a2317fe78ec8305d2bd5a16c554e4c0809ecbe09
Nasario Luna <nasario.luna@yahoo.com>
2020-08-12 11:35:16 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWord = "heck";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("This message is spam");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("This message is not spam");
+            }
+        }
+    }
+}
